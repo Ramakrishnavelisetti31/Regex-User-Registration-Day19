69fde389c0c99e8f439e4342217f85dc2a04c133
ramakrishnavelisetti <ramakrishna96.velisetti@gmail.com>
2021-10-21 20:02:43 +0530

    Added : created a regex pattern for validating user firstname

--- src/main/java/com/bdlz/regexuser/RegexUserRegistration.java
+++ src/main/java/com/bdlz/regexuser/RegexUserRegistration.java
@@ -1,7 +1,22 @@
 package com.bdlz.regexuser;
 
+import java.util.Scanner;
+
 public class RegexUserRegistration {
+
+    Scanner scanner = new Scanner(System.in);
+    UserDetails userDetails = new UserDetails();
+
+    public void userFirstName() {
+        System.out.println("Enter Your First Name : ");
+        String firstName = scanner.next();
+        userDetails.firstName(firstName);
+    }
+
     public static void main(String[] args) {
-        System.out.println("Welcome to the User Registration program by Regex  ");
+        System.out.println("Welcome to the User Registration program by Regex\n");
+
+        RegexUserRegistration regexUserRegistration = new RegexUserRegistration();
+        regexUserRegistration.userFirstName();
     }
 }
