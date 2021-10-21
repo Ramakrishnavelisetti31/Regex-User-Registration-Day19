1f257153ff9ff7a4f879494505ec7bce4948cb72
ramakrishnavelisetti <ramakrishna96.velisetti@gmail.com>
2021-10-21 20:17:20 +0530

    Added : created a regex pattern for validating user emailid

--- src/main/java/com/bdlz/regexuser/RegexUserRegistration.java
+++ src/main/java/com/bdlz/regexuser/RegexUserRegistration.java
@@ -19,11 +19,18 @@ public class RegexUserRegistration {
         userDetails.lastName(lastName);
     }
 
+    public void userEmailId() {
+        System.out.println("Enter Your EmailId : ");
+        String emailId = scanner.next();
+        userDetails.emailId(emailId);
+    }
+
     public static void main(String[] args) {
         System.out.println("Welcome to the User Registration program by Regex\n");
 
         RegexUserRegistration regexUserRegistration = new RegexUserRegistration();
-        regexUserRegistration.userFirstName();
-        regexUserRegistration.userLastName();
+//        regexUserRegistration.userFirstName();
+//        regexUserRegistration.userLastName();
+        regexUserRegistration.userEmailId();
     }
 }
